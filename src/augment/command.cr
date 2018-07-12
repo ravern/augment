616bf66838b8730af3fd298b42a5358949774986
Ravern Koh <ravernkoh@gmail.com>
2018-07-12 10:00:35 +0800

    Add listing and building for RootCommand

--- src/augment/command.cr
+++ src/augment/command.cr
@@ -1,12 +1,36 @@
 class Augment::Command
+  @proxy = true
+
   getter args
   getter flags
 
-  def initialize(@args : Array(String) = ARGV, @input : IO = STDIN, @output : IO = STDOUT, @error : IO = STDERR, &@block : ->)
+  def initialize(@args : Array(String) = ARGV, @input : IO = STDIN, @output : IO = STDOUT, @error : IO = STDERR)
     @parser = Parser.new(@args)
   end
 
   def run
+    command = @args.shift
+
+    with self yield
+
+    if @proxy
+      command = resolve(command)
+      if command
+        Process.exec(command, args: @args, input: @input, output: @output, error: @error)
+      end
+    end
+  end
+
+  # Resolves the path of a command, excluding paths that start with
+  # `~/.augment/bin`.
+  private def resolve(command : String) : String?
+    dir = "#{ENV["HOME"]}/.augment"
+    ENV["PATH"].split(':').each do |path|
+      path = "#{path}/#{command}"
+      if !path.starts_with?("#{dir}/bin") && File.executable?(path)
+        return path
+      end
+    end
   end
 
   def command(name : String)
--- src/augment/root_command.cr
+++ src/augment/root_command.cr
@@ -1,10 +1,38 @@
 class Augment::RootCommand < Augment::Command
+  # Stores the list of command names for use in `list` command.
+  @list : Array(String)?
+
   def run
-    if @args[0] == "list"
-      @block.call
+    if @args.empty?
+      @args = ["build"]
+    end
+
+    command = @args.first
+
+    case command
+    when "build"
+      Builder.new.build
+      return
+    when "list"
+      @list = [] of String
+    end
+
+    super do
+      with self yield
+    end
+
+    if list = @list
+      list.each do |name|
+        @output.puts name
+      end
+    end
+  end
+
+  def command(name : String)
+    if list = @list
+      list << name
     else
-      command = @args.shift
-      Process.exec("/usr/bin/#{command}", @args, input: @input, output: @output, error: @error)
+      super
     end
   end
 end
